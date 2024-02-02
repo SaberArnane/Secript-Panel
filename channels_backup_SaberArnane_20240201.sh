7b6c5f20679029a12f08019a18bc2a7b8a3c47ae
saber <137646541+SaberArnane@users.noreply.github.com>
2024-02-02 19:50:38 +0100

    Update channels_backup_SaberArnane_20240201.sh

--- channels_backup_SaberArnane_20240201.sh
+++ channels_backup_SaberArnane_20240201.sh
@@ -6,7 +6,7 @@ version=motor
 echo "> Downloading "$channel" "$version" Channels List  Please Wait ......"
 sleep 3s
 
-wget -O /var/volatile/tmp/$channel-$"version".tar.gz "https://github.com/SaberArnane/Secript-Panel/raw/main/channels_backup_SaberArnane_20240201.tar.gz"
+wget -O /var/volatile/tmp/$channel-$"version".tar.gz "https://github.com/SaberArnane/Secript-Panel/raw/main/channels_backup_SaberArnane_02022024.tar.gz"
 
 echo ""
 
